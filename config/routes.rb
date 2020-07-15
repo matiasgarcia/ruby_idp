c8182828bb6f2c4b5082a50f6d3dfb39f0ad5e67
matias <garcia.matias92@gmail.com>
2020-07-15 12:27:44 -0300

    Add new configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/saml/auth' => 'saml_idp#new'
+  get '/saml/metadata' => 'saml_idp#show'
   post '/saml/auth' => 'saml_idp#create'
+  match '/saml/logout' => 'saml_idp#logout', via: [:get, :post, :delete]
 end
