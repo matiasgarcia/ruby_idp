002a79a960703257ae4d3e3638b790660db46a79
matias <garcia.matias92@gmail.com>
2020-07-16 16:04:42 -0300

    Remove unused stuff

--- config/initializers/saml_idp.rb
+++ config/initializers/saml_idp.rb
@@ -9,85 +9,13 @@ SamlIdp.configure do |config|
 
   config.secret_key = SamlIdp::Default::SECRET_KEY
 
-  # config.password = "secret_key_password"
-  # config.algorithm = :sha256
-  # config.organization_name = "Your Organization"
-  # config.organization_url = "http://example.com"
-  # config.base_saml_location = "#{base}/saml"
-  # config.reference_id_generator                                 # Default: -> { UUID.generate }
-  # config.single_logout_service_post_location = "#{base}/saml/logout"
-  # config.single_logout_service_redirect_location = "#{base}/saml/logout"
-  # config.attribute_service_location = "#{base}/saml/attributes"
-  # config.single_service_post_location = "#{base}/saml/auth"
-  # config.session_expiry = 86400                                 # Default: 0 which means never
-
-  # Principal (e.g. User) is passed in when you `encode_response`
-  #
+  config.password = "secret_key_password"
+  config.algorithm = :sha256
   config.name_id.formats = {
       email_address: -> (principal) { principal.email },
       transient: -> (principal) { principal.id },
       persistent: -> (p) { p.id },
     }
-  #   OR
-  #
-  #   {
-  #     "1.1" => {
-  #       email_address: -> (principal) { principal.email_address },
-  #     },
-  #     "2.0" => {
-  #       transient: -> (principal) { principal.email_address },
-  #       persistent: -> (p) { p.id },
-  #     },
-  #   }
-
-  # If Principal responds to a method called `asserted_attributes`
-  # the return value of that method will be used in lieu of the
-  # attributes defined here in the global space. This allows for
-  # per-user attribute definitions.
-  #
-  ## EXAMPLE **
-  # class User
-  #   def asserted_attributes
-  #     {
-  #       phone: { getter: :phone },
-  #       email: {
-  #         getter: :email,
-  #         name_format: Saml::XML::Namespaces::Formats::NameId::EMAIL_ADDRESS,
-  #         name_id_format: Saml::XML::Namespaces::Formats::NameId::EMAIL_ADDRESS
-  #       }
-  #     }
-  #   end
-  # end
-  #
-  # If you have a method called `asserted_attributes` in your Principal class,
-  # there is no need to define it here in the config.
-
-  # config.attributes # =>
-  #   {
-  #     <friendly_name> => {                                                  # required (ex "eduPersonAffiliation")
-  #       "name" => <attrname>                                                # required (ex "urn:oid:1.3.6.1.4.1.5923.1.1.1.1")
-  #       "name_format" => "urn:oasis:names:tc:SAML:2.0:attrname-format:uri", # not required
-  #       "getter" => ->(principal) {                                         # not required
-  #         principal.get_eduPersonAffiliation                                # If no "getter" defined, will try
-  #       }                                                                   # `principal.eduPersonAffiliation`, or no values will
-  #    }                                                                      # be output
-  #
-  ## EXAMPLE ##
-  # config.attributes = {
-  #   GivenName: {
-  #     getter: :first_name,
-  #   },
-  #   SurName: {
-  #     getter: :last_name,
-  #   },
-  # }
-  ## EXAMPLE ##
-
-  # config.technical_contact.company = "Example"
-  # config.technical_contact.given_name = "Jonny"
-  # config.technical_contact.sur_name = "Support"
-  # config.technical_contact.telephone = "55555555555"
-  # config.technical_contact.email_address = "example@example.com"
 
   service_providers = {
     "http://localhost:3000" => {
